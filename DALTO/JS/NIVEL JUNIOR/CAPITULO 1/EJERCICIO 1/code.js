6db18275d4cabfede2333a522b7827bfdcc463bc
carlos parra <u2204739@outlook.com>
2025-10-08 08:28:28 -0500

    ejercicio cofla helados

---
+++ DALTO/JS/NIVEL JUNIOR/CAPITULO 1/EJERCICIO 1/code.js
@@ -0,0 +1,121 @@
+dineroCofla = prompt("Cuanto dinero tienes cofla?");
+dineroGonzalo= prompt("Cuanto dinero tienes Gonza?");
+dineroCarlos= prompt("Cuanto dinero tienes Carlos?");
+
+dineroCofla = parseInt(dineroCofla);
+
+if (dineroCofla >= 2000 && dineroCofla < 3000) {
+    alert("cofla, compra el helado de agua")
+    alert("y te sobran" + (dineroCofla - 2500));
+}
+
+else if (dineroCofla >= 3000 && dineroCofla < 4000) {
+    alert("cofla, comprate el helado de crema");
+    alert("y te sobran" + (dineroCofla - 3000));
+}
+
+else if (dineroCofla >= 4000 && dineroCofla < 5000) {
+    alert("cofla, comprate el helado de Polet");
+    alert("y te sobran" + (dineroCofla - 4000));
+}
+
+else if (dineroCofla >= 5000 && dineroCofla < 6000) {
+    alert("cofla, comprate el helado de Bocatto");
+    alert("y te sobran" + (dineroCofla - 5000));
+}
+
+else if (dineroCofla >= 6000 && dineroCofla < 7000) {
+    alert("cofla, comprate el helado de Casero");
+    alert("y te sobran" + (dineroCofla - 6000));
+}
+
+else if (dineroCofla >= 7000 && dineroCofla < 8000) {
+    alert("cofla, comprate el helado de crema");
+    alert("y te sobran" + (dineroCofla - 7000));
+}
+
+else if (dineroCofla >= 8100) {
+    alert("cofla, comprate el helado de litro");
+    alert("y te sobran" + (dineroCofla - 8100));
+
+} else {
+    alert ("Lo siento cofla, no te alcanza para ningun helado");
+}
+
+
+if (dineroGonzalo >= 2000 && dineroGonzalo < 3000) {
+    alert("Gonza, compra el helado de agua")
+    alert("y te sobran" + (dineroGonzalo - 2500));
+}
+
+else if (dineroGonzalo >= 3000 && dineroGonzalo < 4000) {
+    alert("Gonza, comprate el helado de crema");
+    alert("y te sobran" + (dineroGonzalo - 3000));
+}
+
+else if (dineroGonzalo >= 4000 && dineroGonzalo < 5000) {
+    alert("Gonza, comprate el helado de Polet");
+    alert("y te sobran" + (dineroGonzalo - 4000));
+}
+
+else if (dineroGonzalo >= 5000 && dineroGonzalo < 6000) {
+    alert("Gonza, comprate el helado de Bocatto");
+    alert("y te sobran" + (dineroGonzalo - 5000));
+}
+
+else if (dineroGonzalo >= 6000 && dineroGonzalo < 7000) {
+    alert("Gonza, comprate el helado de Casero");
+    alert("y te sobran" + (dineroGonzalo - 6000));
+}
+
+else if (dineroGonzalo >= 7000 && dineroGonzalo < 8000) {
+    alert("Gonza, comprate el helado de crema");
+    alert("y te sobran" + (dineroGonzalo - 7000));
+}
+
+else if (dineroGonzalo >= 8100) {
+    alert("Gonza, comprate el helado de litro");
+    alert("y te sobran" + (dineroGonzalo - 8100));
+
+} else {
+    alert ("Lo siento Gonza, no te alcanza para ningun helado");
+}
+
+
+if (dineroCarlos >= 2000 && dineroCarlos < 3000) {
+    alert("Carlos, compra el helado de agua")
+    alert("y te sobran" + (dineroCarlos - 2500));
+}
+
+else if (dineroCarlos >= 3000 && dineroCarlos < 4000) {
+    alert("Carlos, comprate el helado de crema");
+    alert("y te sobran" + (dineroCarlos - 3000));
+}
+
+else if (dineroCarlos >= 4000 && dineroCarlos < 5000) {
+    alert("Carlos, comprate el helado de Polet");
+    alert("y te sobran" + (dineroCarlos - 4000));
+}
+
+else if (dineroCarlos >= 5000 && dineroCarlos < 6000) {
+    alert("Carlos, comprate el helado de Bocatto");
+    alert("y te sobran" + (dineroCarlos - 5000));
+}
+
+else if (dineroCarlos >= 6000 && dineroCarlos < 7000) {
+    alert("Carlos, comprate el helado de Casero");
+    alert("y te sobran" + (dineroCarlos - 6000));
+}
+
+else if (dineroCarlos >= 7000 && dineroCarlos < 8000) {
+    alert("Carlos, comprate el helado de crema");
+    alert("y te sobran" + (dineroCarlos - 7000));
+}
+
+else if (dineroCarlos >= 8100) {
+    alert("Carlos, comprate el helado de litro");
+    alert("y te sobran" + (dineroCarlos - 8100));
+
+} else {
+    alert ("Lo siento Carlos, no te alcanza para ningun helado");
+}
