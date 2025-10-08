d7d2fd6b492eddedcf8e77472308b6fbf084c8e7
carlos parra <u2204739@outlook.com>
2025-10-08 09:01:48 -0500

    acceso a la fiesta

---
+++ DALTO/JS/NIVEL JUNIOR/CAPITULO 2/EJERCICIO 1/code.js
@@ -0,0 +1,23 @@
+let free = false;
+
+const ValidateClient = (time)=>{
+  let age = prompt("Cual es tu edad?");
+  if (age >= 18) {
+    if (time >= 2 && time < 7 && free == false) {
+      alert("Puedes pasar gratis ya que eres la primer persona despues de las 2 AM");
+      free = true;
+    } else {
+      alert(`son las ${time}:00Hs y puedes pasar, pero tienes que pagar la entrada`);
+    }
+  } else {
+    alert("Eres menor de edad y no puedes pasar");
+  }
+}
+
+ValidateClient(18);
+ValidateClient(19);
+ValidateClient(17);
+ValidateClient(16);
+ValidateClient(20);
+ValidateClient(6);
+ValidateClient(2);
